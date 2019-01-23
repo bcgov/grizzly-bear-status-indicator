1e087d1f320755610bb41b94dd4846d4640f0564
Julie <julie.galloway@gov.bc.ca>
2019-01-23 11:51:30 -0800

    Clean data download commands

--- 01_load.R
+++ 01_load.R
@@ -27,7 +27,7 @@ lapply(Packages, library, character.only = TRUE)
 ## Data is released under the Open Government Licence - British Columbia
 ## https://www2.gov.bc.ca/gov/content?id=A519A56BC2BF44E4A008B33FCF527F61
 
-# Get grizzly pop estimate data (dated version - 2012)
+# Get grizzly pop estimate data (2012)
 bears <- read_csv("https://catalogue.data.gov.bc.ca/dataset/2bf91935-9158-4f77-9c2c-4310480e6c29/resource/4eca8c5c-ed25-46c1-835c-3d9f84b807e1/download/grizzlypopulationestimate2012.csv")
 glimpse(bears)
 
@@ -38,16 +38,5 @@ popunits <- bcdc_get_geodata("grizzly-bear-population-units",
 # Get grizzly mortality data
 bearmort <- read_csv("https://catalogue.data.gov.bc.ca/dataset/4bc13aa2-80c9-441b-8f46-0b9574109b93/resource/c5fc42c7-67d3-4669-b281-61dc50fdef22/download/grizzlybearmortalityhistory_1976_2017.csv")
 
-# Get BC boundary
-bc <- bc_bound()
-plot(st_geometry(bc))
-
 # Get biogeoclimatic zones
 bec <- bec()
-
-# Load grizzly bear population units as an sfc object
-# popunits <- st_read("GCPB_GRIZZLY_BEAR_POP_UNITS_SP.geojson") # saved locally - need to add to bcmaps
-# popunits_sfc <- st_geometry(popunits)
-
-# Alternative method: Reading in geojson as sp object
-# popunits_sp <- geojsonio::geojson_read("GCPB_GRIZZLY_BEAR_POP_UNITS_SP.geojson", what = "sp")
