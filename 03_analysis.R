d548ad65cc2f35a1ec9a9a897e569850f8400513
Julie <julie.galloway@gov.bc.ca>
2019-01-24 11:22:23 -0800

    Edit population and mortality plots

--- 03_analysis.R
+++ 03_analysis.R
@@ -10,22 +10,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-# Assign colour palette
-pal <- c("Extirpated" = "firebrick2", "Threatened" = "yellow1", "Viable" = "forestgreen")
+# Create colour palette for future mapping (not currently used)
+# pal <- c("Extirpated" = "firebrick2", "Threatened" = "yellow1", "Viable" = "forestgreen")
 
 # Build basic static map for grizzly popunits/status
 staticmap <- ggplot(popunits_xy) +
-  geom_sf(aes(fill = STATUS), color = "white", size = 0.1) +
+  geom_sf(aes(fill = status), color = "white", size = 0.1) +
   labs(title = "Conservation Status of Grizzly Bear Population Units in BC") +
   scale_fill_viridis(discrete = T, alpha = 0.8, direction = -1) +
   theme_minimal() +
-  geom_text_repel(aes(label = POPULATION_NAME, x = lng, y = lat),
+  geom_text_repel(aes(label = population_name, x = lng, y = lat),
                   size = 2, force =  0.5) # Needs some tweaking - some labels off polygons
 staticmap # plot map
 
 # Plot basic POPULATION estimate per management unit
 popplot <- ggplot(by_gbpu) +
-  geom_col(aes(x = reorder(POPULATION_NAME, -Estimate), y = Estimate)) +
+  geom_col(aes(x = reorder(population_name, -pop_estimate), y = pop_estimate)) +
   theme_soe() +
   scale_y_continuous("Population Estimate") +
   scale_x_discrete("Population Unit") +
@@ -36,7 +36,7 @@ popplot # Display plot
 
 # Plot basic DENSITY estimate per management unit
 densplot <- ggplot(by_gbpu) +
-  geom_col(aes(x = reorder(POPULATION_NAME, -Density), y = Density)) +
+  geom_col(aes(x = reorder(population_name, -pop_density), y = pop_density)) +
   theme_soe() +
   scale_y_continuous("Population Density Estimate") +
   scale_x_discrete("Population Unit") +
@@ -47,12 +47,15 @@ densplot # Display plot
 
 # Build  static grizzly  density choropleth
 grizzlydensmap <- ggplot(popunits_xy) +
-  geom_sf(aes(fill = Density)) +
-  labs(title = "Grizzly Bear Population Density in British Columbia") +
-  theme_minimal() + theme(plot.title = element_text(hjust = 0.5)) +
-  scale_fill_viridis_c(trans = "sqrt", alpha = .5) +
-  geom_text_repel(aes(label = POPULATION_NAME, x = lng, y = lat),
-                  size = 2, force =  0.5)
+  geom_sf(aes(fill = pop_density)) +
+  labs(title = "Grizzly Bear Population Density in British Columbia",
+       fill = "Population Density") + # Legend title
+  theme_minimal() + theme(plot.title = element_text(hjust = 0.5), # Center main title
+                          axis.title.x = element_blank(), # Remove xy labels
+                          axis.title.y = element_blank()) +
+  scale_fill_viridis_c(trans = "sqrt", alpha = .5) + # Set colour (viridis)
+  geom_text_repel(aes(label = population_name, x = lng, y = lat),
+                  size = 2, force =  0.5) # Offset labels
   #geom_text(aes(label = POPULATION_NAME, x = lng, y = lat),
             #position = position_dodge(width = 0.8), size = 3) # Needs some tweaking - some labels off polygons
 grizzlydensmap # plot map
@@ -63,11 +66,14 @@ grizzlydensmap # plot map
 
 # Build static grizzly population choropleth
 grizzlypopmap <- ggplot(popunits_xy) +
-  geom_sf(aes(fill = Estimate)) +
-  labs(title = "Grizzly Bear Population Estimates for British Columbia") +
-  theme_minimal() + theme(plot.title = element_text(hjust = 0.5)) +
+  geom_sf(aes(fill = pop_estimate)) +
+  labs(title = "Grizzly Bear Population Estimates for British Columbia",
+       fill = "Population Estimate") +
+  theme_minimal() + theme(plot.title = element_text(hjust = 0.5),
+                          axis.title.x = element_blank(),
+                          axis.title.y = element_blank()) +
   scale_fill_viridis_c(trans = "sqrt", alpha = .5) +
-  geom_text_repel(aes(label = POPULATION_NAME, x = lng, y = lat),
+  geom_text_repel(aes(label = population_name, x = lng, y = lat),
                   size = 2, force =  0.5)
 grizzlypopmap # plot map
 
@@ -77,25 +83,34 @@ grizzlypopmap # plot map
 
 # Summarise # of bears killed per kill type + management unit
 mort_gbpu <- bearmort %>%
-  group_by(GBPU_NAME, KILL_CODE, HUNT_YEAR) %>%
-  summarise(COUNT = n())
+  group_by(gbpu_name, kill_code, hunt_year) %>%
+  summarise(count = n())
 glimpse(mort_gbpu)
 
 # Create colour palette for all the plots
-chartFill <- brewer.pal(7, "Set2")
-names(chartFill) <- levels(mort_gbpu$KILL_CODE)
+chartFill <- brewer.pal(7, "Dark2")
+names(chartFill) <- levels(mort_gbpu$kill_code)
 plot.fillScale <- scale_fill_manual(values=chartFill)
 
+# Caption text
+caption.text <- paste("*Note that prior to 2004, road and rail kills",
+                      " were not distinguished and were documented with",
+                      " 'Pick Ups'\nA Limited Entry Hunt (LEH) was",
+                      " instituted province-wide for grizzly bears in",
+                      " 1996.\nThere was a province-wide moratorium on",
+                      " hunting grizzly bears in the spring of 2001."
+                      , sep="")
+
 # Plot for basic POPULATION estimate per management unit
-mortplot <- ggplot(mort_gbpu, aes(x = HUNT_YEAR, y = COUNT,
-                            group = KILL_CODE, fill = KILL_CODE)) +
+mortplot <- ggplot(mort_gbpu, aes(x = hunt_year, y = count,
+                            group = kill_code, fill = kill_code)) +
   geom_bar(stat = "identity") + # Add bar for each year w/ fill = kill type
-  theme_soe() +
-  scale_fill_brewer(type = "seq", palette = "Set2") +
+  theme_bw() +
+  scale_fill_brewer(type = "qual", palette = "Set2") +
   scale_x_continuous(breaks=seq(1970, 2017, by = 5)) +
   labs(title = "Grizzly Bear Mortality for the Province of BC, 1976-2017", x = "Year",
        y = "Number of Grizzly Bears Killed", fill = "Mortality Type") +
-  theme(plot.title = element_text(hjust = 0.5))
-mortplot
-
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "bottom") +
+  labs(caption = caption.text)
+mortplot # Display figure
 
