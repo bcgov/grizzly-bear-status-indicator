b767e897cb80002f8dd0e546163ef1fd0f1bd45b
Julie <julie.galloway@gov.bc.ca>
2019-01-24 11:23:02 -0800

    Add comments to graphing function

--- 04_output.R
+++ 04_output.R
@@ -20,36 +20,38 @@ plotlist <- list()
 Mortality <- function(mort_gbpu) {
 
   # Create list of GBPU
-  gbpu_list <- unique(mort_gbpu$GBPU_NAME)
+  gbpu_list <- unique(mort_gbpu$gbpu_name)
 
   # Create ggplot graph loop
   for (i in seq_along(gbpu_list)) {
 
     # Create plot for each GBPU
     plot <-
-      ggplot(subset(mort_gbpu, mort_gbpu$GBPU_NAME == gbpu_list[i]),
-                    aes(x = HUNT_YEAR, y = COUNT,
-                        fill = KILL_CODE)) +
+      ggplot(subset(mort_gbpu, mort_gbpu$gbpu_name == gbpu_list[i]),
+                    aes(x = hunt_year, y = count,
+                        fill = kill_code)) +
                geom_bar(stat = "identity") + # Add bar for each year w/ fill = kill type
                scale_fill_brewer("Mortality Type", palette = "Set2") +
                scale_x_continuous(breaks=seq(1970, 2017, by = 5)) +
                labs(x = "Year", y = "Number of Grizzly Bears Killed",
-                    fill = "Mortality Type") +
+                    fill = "Mortality Type") + # Legend text
                ggtitle(paste("Mortality History for the '"
                              ,gbpu_list[i]
                              , "' Population Unit"
                              , ", 1976-2017"
                              ,sep = "")) +
-      theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+      theme_bw() + theme(plot.title = element_text(hjust = 0.5), # Centre main title
+                         legend.position = "bottom")
+
     plotlist <- c(plotlist, list(plot))
 
     # Print plots
-    #print(plot)
+    print(plot)
 
     # Save
-    ggsave(plot, file = paste(results,
-                              'grizzly_graphs/',
-                              gbpu_list[i]), plot = image, width = 10, height = 8)
+    #ggsave(plot, file = paste(results,
+     #                         'grizzly_graphs/',
+      #                        gbpu_list[i]), plot = image, width = 10, height = 8)
 
   }
 }
