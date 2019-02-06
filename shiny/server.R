0a60441810c5eb3bc5f0b938779bff01caf9a7cf
Julie <julie.galloway@gov.bc.ca>
2019-02-06 13:25:25 -0800

    update leaflet on shiny map

--- shiny/server.R
+++ shiny/server.R
@@ -2,25 +2,27 @@
 server <- function(input, output, session) {
 
   output$grizzmap <- renderLeaflet({
-    grizzmap <- leaflet() %>%     # generate leaflet
+    grizzmap <- leaflet(grizzdata_full, width = "900px", height = "500px") %>%   # generate leaflet map
       addProviderTiles(providers$Stamen.TerrainBackground, group = "Terrain") %>%
       addTiles(group = "OSM (Default") %>%
-      addLegend("bottomright", pal = factpal, values = grizzdata$status,
+      add_bc_home_button() %>%
+      set_bc_view_on_close() %>% # re-centre map on popup close
+      addLegend("bottomright", pal = factpal, values = grizzdata_full$status,
                 title = "Population Status",
                 opacity = 1) %>%
-      addPolygons(data = grizzdata,
-                  stroke = T, weight = 1, color = "black", # Add border to polygons
+      addPolygons(stroke = T, weight = 1, color = "black", # Add border to polygons
                   fillOpacity = 0.4, # Polygon fill
-                  fillColor = ~factpal(grizzdata$status),
+                  fillColor = ~factpal(grizzdata_full$status),
+                  popup = popups,
+                  popupOptions = popup_options,
+                  label = plotlabs,
+                  labelOptions = labelOptions(direction = "auto", textsize = "12px"),
                   highlight = highlightOptions( # Highlight interaction for mouse hover
                     weight = 4,
                     color = "yellow",
                     bringToFront = T)) %>%
       addLayersControl(
-        baseGroups = c("Terrain", "OSM (Default")) %>%
-      addMarkers(data = grizzdata, lng = ~lng, lat = ~lat,
-                 label = grizzdata$population_name, icon = glyphtree,
-                 labelOptions = labelOptions(noHide = F, textOnly = F))
+        baseGroups = c("Terrain", "OSM (Default"))
   })
 }
 
