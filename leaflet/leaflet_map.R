e6fdc840eeac598d7bccdbe1e3436a4e8fe46d10
Julie <julie.galloway@gov.bc.ca>
2019-02-06 12:07:53 -0800

    add working popups to leaflet

--- leaflet/leaflet_map.R
+++ leaflet/leaflet_map.R
@@ -1,6 +1,6 @@
-##**************
+## --------
 ## LEAFLET
-##**************
+## --------
 
 # Create custom icons
 tree <- makeAwesomeIcon(
@@ -8,31 +8,50 @@ tree <- makeAwesomeIcon(
   iconColor = 'white')
 
 # Prep - colour palettes
-factpal <- colorFactor(palette = 'magma', popunits_xy$status) # Assign scheme
+factpal <- colorFactor(palette = 'magma', grizzdata_full$status) # Assign scheme
 
-##********
+# gbpu_plots <- st_read(here("./out"), stringsAsFactors = FALSE)))
+
+## ------
+## POPUPS
+## -------
+grizz_plotlist <- readRDS(here("out/grizz_plotlist.rds"))[grizzdata_full$population_name]
+popups <-  popupGraph(grizz_plotlist, type = "svg", width = 700,
+                      height = 400)
+popup_options <-  popupOptions(maxWidth = "100%", autoPan = TRUE,
+                               keepInView = TRUE,
+                               closeOnClick = TRUE,
+                               autoPanPaddingTopLeft = c(120, 10),
+                               autoPanPaddingBottomRight = c(10,10))
+require(htmltools)
+plotlabs <- sprintf(
+  "<strong>%s</strong>",
+  tools::toTitleCase(tolower(grizzdata_full$population_name))
+) %>% lapply(htmltools::HTML)
+
+## ------
 ## SETUP
-##********
-grizzmap <- leaflet() %>%   # generate leaflet map
+## ------
+
+grizzmap <- leaflet(grizzdata_full, width = "900px", height = "500px") %>%   # generate leaflet map
   addProviderTiles(providers$Stamen.TerrainBackground, group = "Terrain") %>%
   addTiles(group = "OSM (Default") %>%
   add_bc_home_button() %>%
   set_bc_view_on_close() %>% # re-centre map on popup close
-  addLegend("bottomright", pal = factpal, values = grizzdata$status,
+  addLegend("bottomright", pal = factpal, values = grizzdata_full$status,
             title = "Population Status",
             opacity = 1) %>%
-  addPolygons(data = grizzdata,
-              stroke = T, weight = 1, color = "black", # Add border to polygons
+  addPolygons(stroke = T, weight = 1, color = "black", # Add border to polygons
               fillOpacity = 0.4, # Polygon fill
-              fillColor = ~factpal(grizzdata$status),
+              fillColor = ~factpal(grizzdata_full$status),
+              popup = popups,
+              popupOptions = popup_options,
+              label = plotlabs,
+              labelOptions = labelOptions(direction = "auto", textsize = "12px"),
               highlight = highlightOptions( # Highlight interaction for mouse hover
                 weight = 4,
                 color = "yellow",
                 bringToFront = T)) %>%
   addLayersControl(
-    baseGroups = c("Terrain", "OSM (Default")) %>%
-  addMarkers(data = grizzdata, group = "Population Unit",lng = ~lng, lat = ~lat,
-             label = ~population_name, icon = tree,
-             labelOptions = labelOptions(noHide = F, textOnly = F, sticky = F))
+    baseGroups = c("Terrain", "OSM (Default"))
 grizzmap # View leaflet
-
