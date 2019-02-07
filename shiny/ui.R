84e164d524fcbe35117905a2ba62cba57b0cd6f0
Julie <julie.galloway@gov.bc.ca>
2019-02-06 16:30:04 -0800

    fix tabpanel structure in ui

--- shiny/ui.R
+++ shiny/ui.R
@@ -15,22 +15,26 @@ ui <- navbarPage(title = div(img(src = "http://www2.gov.bc.ca/assets/gov/home/go
                             tabPanel("Summary"))
 )
 
+library(DT)
 # UI with custom CSS
 ui <- function() {
   navbarPage(
     title = div(img(src = "http://www2.gov.bc.ca/assets/gov/home/gov3_bc_logo.png",
                     height = 30, style = "padding: 1px 0px, width: '100%'"),
                 "Grizzly Bear Conservation Status in British Columbia"),
-    tabPanel("Interactive Map"),
-    tabPanel("Data Explorer"),
-    mainPanel(leafletOutput(outputId = "grizzmap")),
+    tabPanel("Interactive Map",
+             icon = icon("globe"),
+             mainPanel(leafletOutput(outputId = "grizzmap"))),
+    tabPanel("Data Explorer",
+             icon = icon("bar-chart"),
+             mainPanel(DT::datatable(mort_summary))),
     navbarMenu("More",
                tabPanel("About"),
                tabPanel("Summary")),
     tags$style(HTML(
                '.navbar { background-color: #003366;}', # navbar background
                '.navbar-default .navbar-brand {color: white; font.family: Arial}', # navbar text col
-               '.navbar-default .navbar-nav > li > a {color:white}', #
+               '.navbar-default .navbar-nav > li > a {color:white}', # navbar text color
                '.navbar { border-bottom: 2px solid #fcba19;}',
                '.navbar .nav-tabs { background-color: #5475a7;}',
                '.tabbable > .nav > li > a { background-color: #5475a7; color: #fff }',
