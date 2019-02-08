c5fbe256e9ecfc8bbea6d11537a68ae23eb6f14f
Julie <julie.galloway@gov.bc.ca>
2019-02-08 15:40:48 -0800

    add layer for population estimate polygons

--- shiny/server.R
+++ shiny/server.R
@@ -7,13 +7,14 @@ server <- function(input, output, session) {
       addTiles(group = "OSM (Default") %>%
       add_bc_home_button() %>%
       set_bc_view() %>%
-      set_bc_view_on_close() %>% # re-centre map on popup close
       addLegend("bottomright", pal = factpal, values = grizzdata_full$status,
-                title = "Population Status",
-                opacity = 1) %>%
+                title = "Conservation Status", opacity = 1) %>%
+      addLegend("bottomleft", pal = poppal, values = grizzdata_full$pop_estimate,
+                title = "Population Estimate", opacity = 1) %>%
       addPolygons(stroke = T, weight = 1, color = "black", # Add border to polygons
                   fillOpacity = 0.4, # Polygon fill
                   fillColor = ~factpal(grizzdata_full$status),
+                  group = "Mortality",
                   popup = popups,
                   popupOptions = popup_options,
                   label = plotlabs,
@@ -22,8 +23,21 @@ server <- function(input, output, session) {
                     weight = 3,
                     color = "yellow",
                     bringToFront = T)) %>%
+      addPolygons(stroke = T, weight = 1, color = "black",
+                  fillOpacity = 0.4,
+                  fillColor = ~poppal(grizzdata_full$pop_estimate),
+                  group = "Population Estimate",
+                  label = plotlabs,
+                  popup = popups,
+                  popupOptions = popup_options,
+                  highlight = highlightOptions( # Highlight interaction for mouse hover
+                    weight = 3,
+                    color = "yellow",
+                    bringToFront = T)) %>%
       addLayersControl(
-        baseGroups = c("Terrain", "OSM (Default"))
+        baseGroups = c("Terrain", "OSM (Default"),
+        overlayGroups = c("Conservation Status", "Population Estimate")
+        )
   })
   leafletOutput('grizzmap', width = "100%")
 }
