1decce7a080215ab2ea3619933fe2e3d0b7ee591
Julie <julie.galloway@gov.bc.ca>
2019-04-01 14:49:53 -0700

    update leaflet map with 2019 pop data

--- leaflet/leaflet_map.R
+++ leaflet/leaflet_map.R
@@ -1,6 +1,8 @@
 ## --------
 ## LEAFLET
 ## --------
+library(bcmaps)
+available_layers()
 
 # Create custom icons
 tree <- makeAwesomeIcon(
@@ -8,16 +10,15 @@ tree <- makeAwesomeIcon(
   iconColor = 'white')
 
 # Prep - colour palettes
-factpal <- colorFactor(palette = 'viridis', grizzdata_full$status, reverse = TRUE) # Assign scheme
-
-# Additional palettes
-poppal <- colorBin(palette = "viridis", grizzdata_full$pop_estimate,
-                   pretty = TRUE, reverse = TRUE, na.color = "#808080")
+palette1 <- colorFactor(palette = 'viridis', grizzdata_full$rankcode,
+                       reverse = TRUE, na.color = "#808080")
+palette2 <- colorFactor(palette = 'viridis', grizzdata_full$adults,
+                        reverse = TRUE, na.color = "#808080")
 
 ## ------
 ## POPUPS
 ## -------
-grizz_plotlist <- readRDS(here("out/grizz_plotlist.rds"))[grizzdata_full$population_name]
+grizz_plotlist <- readRDS(here("out/grizz_plotlist.rds"))[grizzdata_full$gbpu_name]
 popups <-  popupGraph(grizz_plotlist, type = "svg", width = 500,
                       height = 300)
 popup_options <-  popupOptions(maxWidth = "100%", autoPan = TRUE,
@@ -28,26 +29,37 @@ popup_options <-  popupOptions(maxWidth = "100%", autoPan = TRUE,
 require(htmltools)
 plotlabs <- sprintf(
   "<strong>%s</strong>",
-  tools::toTitleCase(tolower(grizzdata_full$population_name))
+  tools::toTitleCase(tolower(grizzdata_full$gbpu_name))
 ) %>% lapply(htmltools::HTML)
 
 ## ------
-## SETUP
+## LEAFLET MAP -- POPULATION AND CONSERVATION STATUS
 ## ------
 grizzmap <- leaflet(grizzdata_full, width = "900px", height = "500px") %>%   # generate leaflet map
   addProviderTiles(providers$Stamen.TerrainBackground, group = "Terrain") %>%
   addTiles(group = "OSM (Default") %>%
   add_bc_home_button() %>%
   set_bc_view() %>%
-  set_bc_view_on_close() %>% # re-centre map on popup close
-  addLegend("bottomright", pal = factpal, values = grizzdata_full$status,
-            title = "Population Status",
+  set_bc_view_on_close() %>% # re-centre map on popup close - do we want this?
+  addLegend("bottomright", pal = palette1, values = grizzdata_full$rankcode,
+            title = "Conservation Status",
             opacity = 1) %>%
   addPolygons(stroke = T, weight = 1, color = "black", # Add border to polygons
-              fillOpacity = 0.5, # Polygon fill
-              fillColor = ~factpal(grizzdata_full$status),
-              popup = popups,
-              popupOptions = popup_options,
+              fillOpacity = 0.4, # Polygon fill
+              fillColor = ~palette1(grizzdata_full$rankcode),
+              #popup = popups,
+              #popupOptions = popup_options,
+              group = "Conservation Rank",
+              label = plotlabs,
+              labelOptions = labelOptions(direction = "auto", textsize = "12px"),
+              highlight = highlightOptions( # Highlight interaction for mouse hover
+                weight = 3,
+                color = "yellow",
+                bringToFront = T)) %>%
+  addPolygons(stroke = T, weight = 1, color = "black",
+              fillOpacity = 0.4,
+              fillColor = ~palette2(grizzdata_full$adults),
+              group = "Population Estimate",
               label = plotlabs,
               labelOptions = labelOptions(direction = "auto", textsize = "12px"),
               highlight = highlightOptions( # Highlight interaction for mouse hover
@@ -55,5 +67,10 @@ grizzmap <- leaflet(grizzdata_full, width = "900px", height = "500px") %>%   # g
                 color = "yellow",
                 bringToFront = T)) %>%
   addLayersControl(
-    baseGroups = c("Terrain", "OSM (Default"))
+    baseGroups = c("Terrain", "OSM (Default"),
+    overlayGroups = c("Conservation Status", "Population Estimate"))
 grizzmap # View leaflet
+
+## ------------------------------
+## LEAFLET MAP -- THREAT MAPPING
+## ------------------------------
