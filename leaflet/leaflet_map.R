e472741e5e1a27afb64fb23211e895f18225403a
Julie <julie.galloway@gov.bc.ca>
2019-04-01 15:56:50 -0700

    create leaflet map for threat calculator data

--- leaflet/leaflet_map.R
+++ leaflet/leaflet_map.R
@@ -11,7 +11,7 @@ tree <- makeAwesomeIcon(
 
 # Prep - colour palettes
 palette1 <- colorFactor(palette = 'viridis', grizzdata_full$rankcode,
-                       reverse = TRUE, na.color = "#808080")
+                        reverse = TRUE, na.color = "#808080")
 palette2 <- colorFactor(palette = 'viridis', grizzdata_full$adults,
                         reverse = TRUE, na.color = "#808080")
 
@@ -74,3 +74,56 @@ grizzmap # View leaflet
 ## ------------------------------
 ## LEAFLET MAP -- THREAT MAPPING
 ## ------------------------------
+tpalette1 <- colorFactor(palette = 'viridis', grizzdata_full$transportationcalc,
+                         reverse = TRUE, na.color = "#808080")
+tpalette2 <- colorFactor(palette = 'viridis', grizzdata_full$energycalc,
+                         reverse = TRUE, na.color = "#808080")
+tpalette3 <- colorFactor(palette = 'viridis', grizzdata_full$humanintrusioncalc,
+                         reverse = TRUE, na.color = "#808080")
+
+# Generate leaflet map
+threatmap <- leaflet(grizzdata_full, width = "900px", height = "500px") %>%
+  addProviderTiles(providers$Stamen.TerrainBackground, group = "Terrain") %>%
+  addTiles(group = "OSM (Default") %>%
+  add_bc_home_button() %>%
+  set_bc_view() %>%
+  set_bc_view_on_close() %>% # re-centre map on popup close
+  #addLegend("bottomright", pal = tpalette1, values = grizzdata_full$transportationcalc,
+            #title = "Transportation Threats",
+            #opacity = 1) %>%
+  addPolygons(stroke = T, weight = 1, color = "black", # Add border to polygons
+              fillOpacity = 0.4, # Polygon fill
+              fillColor = ~tpalette1(grizzdata_full$transportationcalc),
+              #popup = popups,
+              #popupOptions = popup_options,
+              group = "Transportation Threat",
+              label = plotlabs,
+              labelOptions = labelOptions(direction = "auto", textsize = "12px"),
+              highlight = highlightOptions( # Highlight interaction for mouse hover
+                weight = 3,
+                color = "yellow",
+                bringToFront = T)) %>%
+  addPolygons(stroke = T, weight = 1, color = "black",
+              fillOpacity = 0.4,
+              fillColor = ~tpalette2(grizzdata_full$energycalc),
+              group = "Energy Threat",
+              label = plotlabs,
+              labelOptions = labelOptions(direction = "auto", textsize = "12px"),
+              highlight = highlightOptions(
+                weight = 3,
+                color = "yellow",
+                bringToFront = T)) %>%
+  addPolygons(stroke = T, weight = 1, color = "black",
+              fillOpacity = 0.4,
+              fillColor = ~tpalette3(grizzdata_full$humanintrusioncalc),
+              group = "Human Intrusion Threat",
+              label = plotlabs,
+              labelOptions = labelOptions(direction = "auto", textsize = "12px"),
+              highlight = highlightOptions(
+                weight = 3,
+                color = "yellow",
+                bringToFront = T)) %>%
+  addLayersControl(
+    baseGroups = c("Terrain", "OSM (Default"),
+    overlayGroups = c("Transportation Threat", "Energy Threat", "Human Intrusion Threat"))
+threatmap # View leaflet
