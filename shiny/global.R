685d5a88e0fa4d9e7930b34bb09097c106e7f3f5
Julie <julie.galloway@gov.bc.ca>
2019-05-30 13:18:14 -0700

    del original leaflet and shiny folders

--- shiny/global.R
+++
@@ -1,8 +0,0 @@
-# Loading R libraries
-Packages <- c("tidyverse", "shiny", "devtools", "bcmaps",
-              "ggplot2", "leaflet", "rmapshaper", "viridis",
-              "bcdata", "envreportutils","ggspatial", "ggrepel", "htmltools")
-lapply(Packages, library, character.only = TRUE)
-
-# Custom icon from glyphicon
-tree <- icon("tree-conifer", lib = "glyphicon")
