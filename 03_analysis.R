f631a6bce891beccccbfa0e68d30a4a05c0d462e
Julie <julie.galloway@gov.bc.ca>
2019-07-10 12:55:15 -0700

    move table popup generation chunks to output script

--- 03_analysis.R
+++ 03_analysis.R
@@ -10,7 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-## STATIC MAPPING -------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# STATIC MAPPING -------------------------------------------------------------
 grizzdata_full <- st_transform(grizzdata_full, crs = 4326) # convert to lat/long
 
 grizzdata_full <- mutate(grizzdata_full,
@@ -64,31 +65,3 @@ plot(static_ggmap)
 
 # Clip + mask raster to BC boundary
 # stamenbc_crop <- raster::crop(stamenbc, bc_boundary)
-
-## ----------------------------------------------------------------------------
-
-gbpu_list <- unique(threats$gbpu_name)
-
-gbpu_table <- function(data) {
-  st_geometry(data) = NULL
-  gather(data, key = Threat, value = Rank)
-}
-
-table_list <- map(gbpu_list, ~ {
-  data = filter(threats, gbpu_name == .x)
-  gbpu_table(data)
-})
-
-names(table_list) <- gbpu_list
-
-# plot_list <- plot_list[names(table_list)]
-
-plot_list_df <- tibble(
-  popup_row1 = table_list
-  # popup_row2 = plot_list
-)
-
-full_popup <- popup_combine_rows(plot_list_df)
-saveRDS(full_popup, file = "out/full_popup.rds")
-saveRDS(grizzdata_full, "data/grizzdata_full.rds")
-
