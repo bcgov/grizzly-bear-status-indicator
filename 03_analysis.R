24420036e5a898bdbc94ec07365a6e5fd543a10e
Julie <julie.galloway@gov.bc.ca>
2019-07-10 11:28:47 -0700

    move static gbou population plots to scratch

--- 03_analysis.R
+++ 03_analysis.R
@@ -65,62 +65,7 @@ plot(static_ggmap)
 # Clip + mask raster to BC boundary
 # stamenbc_crop <- raster::crop(stamenbc, bc_boundary)
 
-## POPULATION ESTIMATE PLOTTING ------------------------------------------------
-# Plot basic POPULATION estimate per management unit
-popplot <- ggplot(grizzdata_full) +
-  geom_col(aes(x = reorder(gbpu_name, -adults), y = adults)) +
-  theme_soe() +
-  scale_y_continuous("Population Estimate") +
-  scale_x_discrete("Population Unit") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0)) + # rotate labels
-  ggtitle("Grizzly Bear Population Estimate per Unit") +
-  theme(plot.title = element_text(hjust = 0.5))
-popplot # Display plot
-
-# Build static grizzly population choropleth
-grizzlypopmap <- ggplot(grizzdata_full) +
-  geom_sf(aes(fill = adults)) +
-  labs(title = "Grizzly Bear Population Estimates for British Columbia",
-       fill = "Population Estimate") +
-  theme_minimal() + theme(plot.title = element_text(hjust = 0.5),
-                          axis.title.x = element_blank(),
-                          axis.title.y = element_blank()) +
-  scale_fill_viridis_c(trans = "sqrt", alpha = .5, na.value = "darkgrey") +
-  geom_text(aes(label = grizzdata_full$gbpu_name, x = grizzdata_full$lng,
-                y = grizzdata_full$lat), size = 2, check_overlap = F)
-grizzlypopmap # plot map
-
-## POPULATION DENSITY MAPPING: May not be needed for updated version ----------
-# Build  static grizzly density choropleth
-grizzlydensmap <- ggplot(grizzdata_full) +
-  geom_sf(aes(fill = as.numeric(pop_density))) +
-  labs(title = "Grizzly Bear Population Density in British Columbia",
-       fill = "Population Density") + # Legend title
-  theme_minimal() + theme(plot.title = element_text(hjust = 0.5), # Center main title
-                          axis.title.x = element_blank(), # Remove xy labels
-                          axis.title.y = element_blank()) +
-  scale_fill_viridis_c(trans = "sqrt", alpha = .5) + # Set colour (viridis)
-  geom_text_repel(aes(label = gbpu_name, x = lng, y = lat),
-                  size = 2, force =  0.5) # Offset labels
-#geom_text(aes(label = gbpu_name, x = lng, y = lat),
-#position = position_dodge(width = 0.8), size = 3) # Needs some tweaking - some labels off polygons
-grizzlydensmap # plot map
-
-# Plot basic density estimate per management unit
-densplot <- ggplot(by_gbpu) +
-  geom_col(aes(x = reorder(gbpu_name, -pop_density), y = pop_density)) +
-  theme_soe() +
-  scale_y_continuous("Population Density Estimate") +
-  scale_x_discrete("Population Unit") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0)) + # rotate labels
-  ggtitle("Grizzly Bear Population Density Estimate per Unit") +
-  theme(plot.title = element_text(hjust = 0.5))
-densplot # Display plot
-
 ## ----------------------------------------------------------------------------
-# Make condensed threat table
-threats <- dplyr::select(grizzdata_full, gbpu_name, energy, transportation, residential,
-                         agriculture, biouse, humanintrusion, climatechange)
 
 gbpu_list <- unique(threats$gbpu_name)
 
