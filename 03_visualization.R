c9b56fc6310b4bbb67041f728b3462c4de6603f9
Julie <julie.galloway@gov.bc.ca>
2019-07-30 11:44:53 -0700

    add line to load grizzly data if not already in local environment

--- 03_visualization.R
+++ 03_visualization.R
@@ -10,6 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+if (!exists("grizzdata_full")) load("data/grizzdata_full.RData")
+
 ## ----------------------------------------------------------------------------
 ## THREAT POPUP MAPPING
 ## ----------------------------------------------------------------------------
