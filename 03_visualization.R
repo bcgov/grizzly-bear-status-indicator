e31547b66f591c2056de2615943bb59feb04a39a
Perkins <genevieve.perkins@gov.bc.ca>
2019-08-14 15:11:11 -0700

    shift static outputs to 04_script

--- 03_visualization.R
+++ 03_visualization.R
@@ -18,6 +18,8 @@ if (!exists("grizzdata_full")) load("data/grizzdata_full.rds")
 
 
 
+
+
 ## ----------------------------------------------------------------------------
 ## THREAT POPUP MAPPING
 ## ----------------------------------------------------------------------------
@@ -61,11 +63,11 @@ Threat_Plots <- function(data, name) {
     scale_fill_brewer("Threat Ranking", palette = "Set2") +
     labs(x = "Threat", y = "Threat Ranking",
          fill = "Ranking") + # Legend text
-    ggtitle(paste(name," Population Unit Threat Ranking", sep = ""))+
+    ggtitle(name)+
     theme(legend.position = "none")
-   # theme_soe() + theme(plot.title = element_text(hjust = 0.5), # Centre title
-   #                    legend.position = "none",
-   #                     plot.caption = element_text(hjust = 0))  # L-align caption
+    theme_soe() + theme(plot.title = element_text(hjust = 0.5), # Centre title
+                       legend.position = "none",
+                        plot.caption = element_text(hjust = 0))  # L-align caption
  # scale_y_discrete (limits = c("Negligible", "Low", "Medium", "High", "Very High"))
  # This line causes problems with no data sites (ie Central Interior)
 
@@ -102,58 +104,7 @@ iwalk(threat_plot_list, ~ save_svg(.x, fname = paste0("out/", .y, ".svg"),
 # Save plots to file
 # saveRDS(threat_plot_list, file = "out/threat_plotlist.rds")
 
-threat_popups <-  leafpop::popupGraph(threat_plot_list, type = "svg",
-                                      width = 250, height = 250)
+threat_popups <-  leafpop::popupGraph(threat_plot_list, type = "svg")#,
+                                      #width = 250, height = 250)
 
 saveRDS(threat_popups, "out/threat_popups.rds")
-
-## ----------------------------------------------------------------------------
-## STATIC MAPPING
-## ----------------------------------------------------------------------------
-staticmap <- ggplot(grizzdata_full) +
-  geom_sf(aes(fill = calcrank), color = "white", size = 0.1) +
-  labs(title = "Conservation Status of Grizzly Bear Population Units in BC",
-       col = "Conservation Rank",
-       fill = "Management Rank") +
-  scale_fill_viridis(alpha = 0.6, discrete = T, option = "viridis",
-                     direction = -1, na.value = "darkgrey") +
-  theme_soe() + theme(plot.title = element_text(hjust = 0.5),
-                      axis.title.x = element_blank(),
-                      axis.title.y = element_blank(),
-                      legend.background = element_rect(
-                        fill = "lightgrey", size = 0.5,
-                        linetype = "solid", colour = "darkgrey")) +
-  geom_text(aes(label = grizzdata_full$gbpu_name, x = grizzdata_full$lng,
-                y = grizzdata_full$lat), size = 2, check_overlap = T) #+
-  #geom_text_repel(aes(label = gbpu_name, x = lng, y = lat), size = 2, force = 0.5) # Needs some tweaking - some labels off polygons
-staticmap # plot map
-
-# Get stamen basemap (terrain)
-stamenbc <- get_stamenmap(bbox = c(-139.658203,48.5,-113.071289,60.261617),
-                          zoom = 7, maptype = "terrain-background",
-                          where = "/dev/stamen/")
-# saveRDS(stamenbc, file = "/dev/stamen.Rds")
-# readRDS(stamenbc)
-plot(stamenbc) # View basemap
-
-# Plot stamen map with terrain basemap
-static_ggmap <- ggmap(stamenbc) + # Generate new map
-  geom_sf(data = grizzdata_full, aes(fill = calcrank), inherit.aes = F,
-          color = "white", size = 0.01) + # plot with boundary
-  #geom_text(aes(label = grizzdata_full$gbpu_name, x = grizzdata_full$lng,
-  #              y = grizzdata_full$lat")) +
-  # geom_label(data = grizzdata_full$gbpu_name) +
-  theme_soe() + scale_fill_viridis(discrete = T, alpha = 0.5,
-                                   option = "viridis", direction = -1,
-                                   na.value = "darkgrey") +
-  labs(title = "Conservation Status of Grizzly Bear Population Units in BC",
-       fill = "Management Rank") +
-  theme(plot.title = element_text(hjust = 0.5), axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.background = element_rect(
-          fill = "lightgrey", size = 0.5, linetype = "solid",
-          colour = "darkgrey"))
-plot(static_ggmap)
-
-# Clip + mask raster to BC boundary
-# stamenbc_crop <- raster::crop(stamenbc, bc_boundary)
