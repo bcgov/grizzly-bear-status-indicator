9e5ad971f9f8f80100993c1e825f0a1c7138989e
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-08-21 14:17:00 -0700

    incorporate radar plots for pop-ups

--- 03_visualization.R
+++ 03_visualization.R
@@ -13,18 +13,96 @@ library(dplyr)
 
 if (!exists("grizzdata_full")) load("data/grizzdata_full.rds")
 
-# Add conservation status popups?
+ifelse(!dir.exists(file.path("out/tab1/")), dir.create(file.path("out/tab1/")), FALSE)
+ifelse(!dir.exists(file.path("out/tab2/")), dir.create(file.path("out/tab2/")), FALSE)
+#ifelse(!dir.exists(file.path("out/tab3/")), dir.create(file.path("out/tab3/")), FALSE)
+
+# Create list of GBPU
+gbpu_list <- unique(grizzdata_full$gbpu_name)
+
+# Create Conservation Concern Popup Plots ---------------------------------
+grizz.df <- as.data.frame(grizzdata_full)
+
+cc_data <- grizz.df %>%
+  mutate(trend_adj = as.numeric(trend) * -1) %>%
+  select(gbpu_name, calcsrank, trend_adj, popiso_rank_adj, threat_rank_adj) %>%
+  gather("metric", "score", -gbpu_name, -calcsrank, trend_adj, popiso_rank_adj, threat_rank_adj) %>%
+  mutate(max_val = case_when(metric == "trend_adj" ~ 1, metric == "popiso_rank_adj" ~ 4, metric == "threat_rank_adj" ~ 2),
+         label = case_when(metric == "trend_adj" ~ "Trend", metric == "popiso_rank_adj" ~ "Population/\nIsolation", metric == "threat_rank_adj" ~ "Threat"),
+         label_pos= case_when(metric == "trend_adj" ~ 2.2, metric == "popiso_rank_adj" ~ 5.5, metric == "threat_rank_adj" ~ 2.8)
+  )
+
+coord_radar <- function (theta = "x", start = 0, direction = 1, clip = "on") {
+  theta <- match.arg(theta, c("x", "y"))
+  r <- if (theta == "x") "y" else "x"
+  ggproto("CordRadar", CoordPolar, theta = theta, r = r, start = start,
+          direction = sign(direction),
+          clip = clip,
+          is_linear = function(coord) TRUE)
+}
+
+# Create radar plot list
+radar_plot_list <- vector(length = length(gbpu_list), mode = "list")
+names(radar_plot_list) <- gbpu_list
 
 
+Radar_Plots <- function(data, name) {
 
+  p <- ggplot(data, aes(x = metric, y = score)) +
+    #facet_wrap(~ gbpu_name) +
+    geom_polygon(aes(group = NA, fill = as.numeric(str_extract(calcsrank, "\\d")),
+                     colour = as.numeric(str_extract(calcsrank, "\\d"))),
+                 alpha = 0.6, size = 2) +
+    geom_errorbar(aes(x = metric, ymin = 0, ymax = max_val),
+                  width = 0.1, colour = "grey40") +
+    scale_colour_viridis_c(direction = -1, guide = "none") +
+    scale_fill_viridis_c(direction = -1, guide = "none") +
+    # scale_y_continuous(expand = expand_scale(mult = 0, add = 0)) +
+    geom_text(aes(x = metric, y = label_pos, label = label),
+              colour = "grey40") +
+    geom_text(aes(label = calcsrank, colour = as.numeric(str_extract(calcsrank, "\\d"))),
+              x = 0.5, y = 2, size = 4) +
+    geom_text(aes(label = gbpu_name),
+              x = 0.5, y = 4.5, size = 5, colour = "grey40") +
+    coord_radar(clip = "off") +
+    theme_void() +
+    theme(plot.margin = unit(c(0,0,0,0), "lines"), strip.text = element_blank())
+ p
 
+}
 
+ggsave("radar_example.png")
+
+# Create ggplot graph loop
+plots <- for (n in gbpu_list) {
+  print(n)
+  data <- filter(cc_data, gbpu_name == n)
+  p <- Radar_Plots(data, n)
+  radar_plot_list[[n]] <- p
+  ggsave(p, file = paste0("out/tab1/", n, ".svg"))
+}
+
+# Svg function
+save_svg <- function(x, fname, ...) {
+  svg_px(file = fname, ...)
+  plot(x)
+  dev.off()
+}
+
+# Save svgs to plot list
+iwalk(radar_plot_list, ~ save_svg(.x, fname = paste0("out/tab1/", .y, ".svg"),
+                                   width = 250, height = 250))
+
+# Save plots to file
+saveRDS(radar_plot_list, file = "out/tab1/radar_plotlist.rds")
+# create popup and save
+concern_popups <-  leafpop::popupGraph(radar_plot_list, type = "svg")
+saveRDS(concern_popups, "out/tab1/concern_popups.rds")
 
 ## ----------------------------------------------------------------------------
 ## THREAT POPUP MAPPING
 ## ----------------------------------------------------------------------------
-# Create 'out' directory
-figsOutDir <- "out"
+figsOutDir <- "out/tab2/"
 
 threat_calc <- threat_calc %>%
   select(gbpu_name, ends_with("calc")) %>%
@@ -48,9 +126,6 @@ total_threats$ranking <- factor(total_threats$ranking, ordered = TRUE,
 
 saveRDS(total_threats, file = "data/total_threats.rds")
 
-# Create list of GBPU
-gbpu_list <- unique(grizzdata_full$gbpu_name)
-
 # Create list for plots
 threat_plot_list <- vector(length = length(gbpu_list), mode = "list")
 names(threat_plot_list) <- gbpu_list
@@ -58,54 +133,47 @@ names(threat_plot_list) <- gbpu_list
 # Create plotting function
 Threat_Plots <- function(data, name) {
   make_plot <- ggplot(data, aes(x = threat, y = ranking,
-                                fill = ranking, color = ranking)) +
+                                fill = ranking)) +
     geom_bar(stat = "identity") + # Add bar for each threat variable
-    scale_colour_viridis_c(direction = -1, guide = "none") +
-    scale_fill_viridis_c(direction = -1, guide = "none") +
-    #scale_fill_brewer("Threat Ranking", palette = "Set2") +
+    scale_colour_viridis(discrete = TRUE, direction = 1) +
+    scale_fill_viridis(discrete = TRUE, direction = 1) +
     labs(x = "Threat", y = "Threat Ranking",
-         fill = "Ranking") + # Legend text
-    ggtitle(name)+
-    theme(legend.position = "none")
+         fill = "Ranking") +
+    ggtitle(name) +
+    theme(legend.position = "none") +
     theme_soe() + theme(plot.title = element_text(hjust = 0.5), # Centre title
                        legend.position = "none",
-                        plot.caption = element_text(hjust = 0))  # L-align caption
- # scale_y_discrete (limits = c("Negligible", "Low", "Medium", "High", "Very High"))
+                        plot.caption = element_text(hjust = 0)) #+  # L-align caption
+  #scale_y_discrete(limits = c("Negligible", "Low", "Medium", "High", "Very High"),
+  #                  drop = FALSE)
+
  # This line causes problems with no data sites (ie Central Interior)
-
   make_plot + coord_flip()
-}
 
-#ifelse(!dir.exists(file.path("out/")), dir.create(file.path("out/")), FALSE)
+}
 
 # Create ggplot graph loop
 plots <- for (n in gbpu_list) {
-  #n <- gbpu_list[56]
   print(n)
   data <- filter(total_threats, gbpu_name == n)
   p <- Threat_Plots(data, n)
   threat_plot_list[[n]] <- p
-  ggsave(p, file = paste0("out/", n, ".svg"))
+  ggsave(p, file = paste0("out/tab2/", n, ".svg"))
 }
 
 # Check result
-threat_plot_list[["Valhalla"]]
-
-# Svg function
-save_svg <- function(x, fname, ...) {
-  svg_px(file = fname, ...)
-  plot(x)
-  dev.off()
- }
+#threat_plot_list[["Valhalla"]]
 
 # Save svgs to plot list
-iwalk(threat_plot_list, ~ save_svg(.x, fname = paste0("out/", .y, ".svg"),
+iwalk(threat_plot_list, ~ save_svg(.x, fname = paste0("out/tab2/", .y, ".svg"),
                             width = 250, height = 250))
 
 # Save plots to file
-# saveRDS(threat_plot_list, file = "out/threat_plotlist.rds")
+saveRDS(threat_plot_list, file = "out/tab2/threat_plotlist.rds")
 
 threat_popups <-  leafpop::popupGraph(threat_plot_list, type = "svg")#,
                                       #width = 250, height = 250)
 
-saveRDS(threat_popups, "out/threat_popups.rds")
+saveRDS(threat_popups, "out/tab2/threat_popups.rds")
+
+
