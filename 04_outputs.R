b42dae7aeb1476593850df0e7a9cf4e0a34bf046
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-08-21 17:18:22 -0700

    tidy file

--- 04_outputs.R
+++ 04_outputs.R
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 library(dplyr)
-library*
 
 if (!exists("grizzdata_full")) load("data/grizzdata_full.rds")
 
