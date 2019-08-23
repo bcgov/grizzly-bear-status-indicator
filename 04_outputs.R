5139d535c5807ff4678cd1f64f5e1b4de4774238
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-08-23 10:50:11 -0700

    Add colour pallet and update plot colours

--- 04_outputs.R
+++ 04_outputs.R
@@ -22,17 +22,36 @@ sdata <- grizzdata_full %>%
   filter(!is.na(threat_class))
 
 sdata <- as.data.frame(sdata)
-sdata <- sdata %>%
-  select(-geometry)
+sdata <- sdata %>% select(-geometry)
 
-#pal1 <- colorFactor(palette = 'viridis', grizzdata_full$threat_class,na.color = "#808080", reverse = TRUE)
+#pal1 <- colorFactor(palette = 'viridis', grizzdata_full$threat_class,
+#                    ordered = TRUE, na.color = "#808080", reverse = TRUE)
+
+palv <- c("Negligible" = "#440154FF", "Low" = "#3B528BFF" ,
+          "Medium" = "#21908CFF", "High" = "#5DC863FF" ,
+          "Very High" = "#FDE725FF", "NA" = "#808080")
+
+palvr <- c("M5"= "#440154FF",  "M4" ="#3B528BFF", "M3" ="#21908CFF",
+           "M2" ="#5DC863FF", "M1" ="#FDE725FF", "NA" = "#808080")
+palvrn <- c( "5" = "#440154FF",  "4" ="#3B528BFF", "3" ="#21908CFF",
+           "2" ="#5DC863FF", "1" ="#FDE725FF", "NA" = "#808080")
+
+colour_table <- tribble(
+  ~calcsrank, ~plot_col,
+  "M1", "#FDE725FF",
+  "M2", "#5DC863FF",
+  "M3", "#21908CFF",
+  "M4", "#3B528BFF",
+  "M5", "#440154FF"
+)
+
+
 
 overall_threat_plot <-
   ggplot(sdata, aes(y = count, x = threat_class)) +
   geom_bar(stat = "identity", aes(fill = as.factor(threat_class)), show.legend = FALSE) +
   scale_x_discrete(limits = c("Negligible", "Low", "Medium", "High", "Very High"))+
-  scale_colour_viridis(discrete = TRUE, direction = -1) +
-  scale_fill_viridis(discrete = TRUE, direction = -1) +
+  scale_fill_manual(values = palv)+
   geom_text(aes(label=count), vjust=0.5, hjust = 2) +
   coord_flip() +
   labs(y = "Number of Grizzly Bear Population Units (GBPU)", x = "Overall Threat")+
@@ -52,7 +71,7 @@ dev.off()
 
 # generate sumary plot per threat
 
-if (!exists("total_threats")) load("data/total_threats.rds")
+if (!exists("total_threats")) load("dataviz/leaflet/threat_plots/total_threats.rds")
 
 tdata <- total_threats %>%
   group_by(threat, ranking) %>%
@@ -60,13 +79,11 @@ tdata <- total_threats %>%
 
 tdata$ranking <- factor(tdata$ranking, ordered = TRUE,
                                 levels = c("Negligible", "Low", "Medium", "High", "Very High"))
-
 threat_sum_plot <-
   ggplot(tdata, aes(y = count, x = threat,
                               fill = ranking)) +
   geom_bar(stat = "identity") +
-  scale_colour_viridis(discrete = TRUE, direction = 1) +
-  scale_fill_viridis(discrete = TRUE, direction = 1) +
+  scale_fill_manual(values = palv) +
   scale_y_continuous(limits = c(0,55), breaks = seq(0,50,10))+
   ggtitle("Estimated Impact of Threat Catergory") +
   labs(x = "Threat", y = "Number of Grizzly Bear Population Units (n = 55)",
@@ -74,8 +91,6 @@ threat_sum_plot <-
   guides(fill= guide_legend (reverse = TRUE))+
   coord_flip() +
   theme_soe()
-
-threat_sum_plot
 
 ## Printing plots for web in SVG formats (and PNG)
 svg_px("./print_ver/threat_sum_plot.svg", width = 500, height = 400)
@@ -89,6 +104,8 @@ dev.off()
 
 
 # output radar plots for all gbpu units for static version
+# does not include extirpated GBPU
+
 cc_data <- grizz.df %>%
   mutate(trend_adj = as.numeric(trend) * -1) %>%
   select(gbpu_name, calcsrank, trend_adj, popiso_rank_adj, threat_rank_adj) %>%
@@ -96,7 +113,10 @@ cc_data <- grizz.df %>%
   mutate(max_val = case_when(metric == "trend_adj" ~ 1, metric == "popiso_rank_adj" ~ 4, metric == "threat_rank_adj" ~ 2),
          label = case_when(metric == "trend_adj" ~ "Trend", metric == "popiso_rank_adj" ~ "Population/\nIsolation", metric == "threat_rank_adj" ~ "Threat"),
          label_pos= case_when(metric == "trend_adj" ~ 2.2, metric == "popiso_rank_adj" ~ 5.5, metric == "threat_rank_adj" ~ 2.8)
-  )
+  ) %>%
+  left_join(colour_table) %>%
+  filter(!is.na(calcsrank))
+
 
 coord_radar <- function (theta = "x", start = 0, direction = 1, clip = "on") {
   theta <- match.arg(theta, c("x", "y"))
@@ -111,13 +131,14 @@ coord_radar <- function (theta = "x", start = 0, direction = 1, clip = "on") {
 rad_plot <- ggplot(cc_data, aes(x = metric, y = score)) +
     facet_wrap(~ gbpu_name) +
     geom_polygon(aes(group = NA, fill = as.numeric(str_extract(calcsrank, "\\d")),
-                     colour = as.numeric(str_extract(calcsrank, "\\d"))),
-                 alpha = 0.6, size = 2) +
-    geom_errorbar(aes(x = metric, ymin = 0, ymax = max_val),
+                      colour = as.numeric(str_extract(calcsrank, "\\d"))),
+                  alpha = 0.6, size = 2) +
+   geom_errorbar(aes(x = metric, ymin = 0, ymax = max_val),
                   width = 0.1, colour = "grey40") +
     scale_colour_viridis_c(direction = -1, guide = "none") +
     scale_fill_viridis_c(direction = -1, guide = "none") +
-    # scale_y_continuous(expand = expand_scale(mult = 0, add = 0)) +
+    #scale_fill_manual(values = palvr) +
+    #scale_y_continuous(expand = expand_scale(mult = 0, add = 0)) +
     #geom_text(aes(x = metric, y = label_pos, label = label),
     #          colour = "grey40") +
     #geom_text(aes(label = calcsrank, colour = as.numeric(str_extract(calcsrank, "\\d"))),
@@ -128,8 +149,11 @@ rad_plot <- ggplot(cc_data, aes(x = metric, y = score)) +
     theme_void() +
     theme(plot.margin = unit(c(0,0,0,0), "lines"), strip.text = element_blank())
 
-    # add a
 rad_plot
+
+# add a legend and overall plot to explain positions.
+
+
 
 ## Printing plots for web in SVG formats (and PNG)
 svg_px("./print_ver/radar_plot.svg", width = 500, height = 400)
